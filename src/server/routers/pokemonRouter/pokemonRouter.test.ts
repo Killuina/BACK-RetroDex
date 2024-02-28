58078ba28cdb551249818086aca7f2756cdc71d6
Cristina Jiménez <cristinajm98@hotmail.com>
2024-02-28 15:40:33 +0100

    Add setupTestAuthorizationData to automatically generate tokens

--- src/server/routers/pokemonRouter/pokemonRouter.test.ts
+++ src/server/routers/pokemonRouter/pokemonRouter.test.ts
@@ -3,18 +3,38 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import request from "supertest";
 import mongoose from "mongoose";
 import { app } from "../..";
+import jwt from "jsonwebtoken";
 import connectDatabase from "../../../database/connectDatabase";
 import { mockUserPokemon } from "../../../mocks/pokemonMocks";
 import { paths } from "../../utils/paths";
 import statusCodes from "../../utils/statusCodes";
 import UserPokemon from "../../../database/models/UserPokemon";
 import { PokemonTypes } from "../../controllers/pokemonControllers/types";
+import { type CustomJwtPayload } from "../../controllers/userControllers/types";
+import User from "../../../database/models/User";
+import { mockUserCredentials } from "../../../mocks/userMocks";
 
-const authorizationHeader =
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2M2ZhMTEzY2RhNTJkZmYyOGIyNjFlMGEiLCJ1c2VybmFtZSI6Ik1hbm9sbyIsImlhdCI6MTY4NzcxMDMxNiwiZXhwIjoxNjg4MzE1MTE2fQ.uETvvA2hkPaa1b-bH5sGE11pzxTTQBtwn5X7UXag7uM";
+const setupTestAuthorizationData = async () => {
+  const { _id, username } = await User.create(mockUserCredentials);
 
-const noBearerAuthorizationHeader =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2M2ZhMTEzY2RhNTJkZmYyOGIyNjFlMGEiLCJ1c2VybmFtZSI6Ik1hbm9sbyIsImlhdCI6MTY3OTQwMzczMCwiZXhwIjoxNjgwMDA4NTMwfQ.yOEQo7goCnEWWJ-7nhTOLnFFBRB6TCuordrrHXD5U9M";
+  const jwtPayload: CustomJwtPayload = {
+    sub: _id.toString(),
+    username,
+  };
+
+  const token = jwt.sign(jwtPayload, process.env.JWT_SECRET!, {
+    expiresIn: "7d",
+  });
+
+  const authorizationHeader = `Bearer ${token}`;
+
+  const noBearerAuthorizationHeader = "";
+
+  return {
+    authorizationHeader,
+    noBearerAuthorizationHeader,
+  };
+};
 
 const {
   pokemon: {
@@ -46,6 +66,7 @@ afterAll(async () => {
 
 afterEach(async () => {
   await UserPokemon.deleteMany();
+  await User.deleteMany();
 });
 
 describe("Given the GET /pokemon endpoint", () => {
@@ -80,6 +101,7 @@ describe("Given the DELETE /pokemon/:userPokemonId endpoint", () => {
   describe("When it receives a request to delete 'Pokamion'", () => {
     test("Then it should respond with okCode and message: 'Pokamion' deleted", async () => {
       const { _id: id } = await UserPokemon.create(mockUserPokemon);
+      const { authorizationHeader } = await setupTestAuthorizationData();
 
       const deletePokamionEndpoint = `${pokemonPath}/delete/${id.toString()}`;
 
@@ -96,6 +118,8 @@ describe("Given the DELETE /pokemon/:userPokemonId endpoint", () => {
 
     describe("When it receives a request to delete a pokémon that doesn't exists", () => {
       test("Then it should respond with internalServer code and message: 'Error deleting pokémon'", async () => {
+        const { authorizationHeader } = await setupTestAuthorizationData();
+
         const expectedErrorMessage = "Error deleting pokémon";
         const nonExistingPokemonId = "640f22f29ef06cb2185232e3";
 
@@ -112,6 +136,7 @@ describe("Given the DELETE /pokemon/:userPokemonId endpoint", () => {
 
     describe("When it receives a request with an invalid id", () => {
       test("Then it should respond with badRequest code and message 'Please enter a valid Id'", async () => {
+        const { authorizationHeader } = await setupTestAuthorizationData();
         const expectedErrorMessage = "Please enter a valid Id";
         const invalidPokemonId = "123";
 
@@ -141,6 +166,9 @@ describe("Given the DELETE /pokemon/:userPokemonId endpoint", () => {
 
     describe("When it receives a request with header Authorization without bearer", () => {
       test("Then it should respond with error 'Invalid token'", async () => {
+        const { noBearerAuthorizationHeader } =
+          await setupTestAuthorizationData();
+
         const response = await request(app)
           .delete(deleteMockPokemonEndpoint)
           .set("Authorization", noBearerAuthorizationHeader)
@@ -155,6 +183,8 @@ describe("Given the DELETE /pokemon/:userPokemonId endpoint", () => {
 describe("Given a POST /pokemon/create endpoint", () => {
   describe("When it receives a request with all needed data to create a Pokemon named 'Pokamion", () => {
     test("Then it should respond with resource created status code and all Pokamion data, including its name'", async () => {
+      const { authorizationHeader } = await setupTestAuthorizationData();
+
       const response = await request(app)
         .post(`${pokemonPath}${createPokemon}`)
         .set("Authorization", authorizationHeader)
@@ -177,6 +207,7 @@ describe("Given a POST /pokemon/create endpoint", () => {
 
   describe("When it receives a request with no name field", () => {
     test("Then it should respond with status 400 and message: 'Validation Failed'", async () => {
+      const { authorizationHeader } = await setupTestAuthorizationData();
       const expectedError = "Validation Failed";
 
       const response = await request(app)
@@ -197,6 +228,8 @@ describe("Given a POST /pokemon/create endpoint", () => {
 
   describe("When it receives a request with a name that already exists in the database", () => {
     test("Then it should respond with bad requests status and message: 'Name already exists'", async () => {
+      const { authorizationHeader } = await setupTestAuthorizationData();
+
       await UserPokemon.create(mockUserPokemon);
 
       const expectedError = "Name already exists";
